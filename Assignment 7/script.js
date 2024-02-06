67954ceb54fcf53d0ad384f9cb5c88140a47fa27
ZachFlores <zjflores@email.sc.edu>
2024-02-05 21:49:15 -0500

    adding assignment 7

--- Assignment 7/script.js
+++ Assignment 7/script.js
@@ -1,5 +1,30 @@
 const changeColor = () => {
-    document.getElementById("image1").style.color = blue;
+    document.getElementById("image1");
+    
+
 };
 
 document.getElementById("image1").onclick = changeColor;
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+
+slider.oninput = function() {
+
+};
+
+const addStar = () => {
+    
+    const div = document.getElementById("col3");
+
+    const starry = document.createElement("section");
+    div.append(starry);
+    starry.classList.add("star");
+    ball.style.background = color;
+
+};
+
+
+document.getElementById("col3").onclick = addStar;
+

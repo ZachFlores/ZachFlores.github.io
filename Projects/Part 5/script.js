1b15ede5bf53cbaa66a06088cf5b4073f139dc51
ZachFlores <zjflores@email.sc.edu>
2024-03-17 18:34:34 -0400

    fixed bugs

--- Projects/Part 5/script.js
+++ Projects/Part 5/script.js
@@ -1,41 +1,41 @@
 const toggleHamburger = () => {
-    document.getElementById("nav-items").classList.toggle("hide");
-  };
-  
-  window.onload = () => {
-    document.getElementById("hamburger").onclick = toggleHamburger;
-    displayServiceList();
-  };
-  
-  const getAttractions = async () => {
-    const url = "data.json";
-    try {
-      const response = await fetch(url);
-      return response.json();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  const displayServiceList = async () => {
-    const serviceListDiv = document.getElementById("service-list");
-    const attractions = await getAttractions();
-  
-    attractions.forEach(attraction => {
-      const serviceItem = document.createElement("div");
-      serviceItem.classList.add("service-item");
-  
-      serviceItem.innerHTML = `
-        <img src="images/${attraction.img_name}" alt="${attraction.title}">
-        <h3>${attraction.title}</h3>
-        <p>${attraction.description}</p>
-        <ul>
-          ${attraction.features.map(feature => `<li>${feature}</li>`).join('')}
-        </ul>
-        <p><strong>Price:</strong> ${attraction.price}</p>
-      `;
-  
-      serviceListDiv.appendChild(serviceItem);
-    });
-  };
-  
+  document.getElementById("nav-items").classList.toggle("hide");
+};
+
+window.onload = () => {
+  document.getElementById("hamburger").onclick = toggleHamburger;
+  displayServiceList();
+};
+
+const getServices = async () => {
+  const url = "JSON/cyber.json";
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayServiceList = async () => {
+  const services = await getServices();
+
+  if (!services) {
+    console.error("Error fetching services data.");
+    return;
+  }
+
+  const serviceListHTML = services.map(service => `
+    <div class="service-item">
+      <img src="images/${service.img_name}" alt="${service.title}">
+      <h3>${service.title}</h3>
+      <p>${service.description}</p>
+      <ul>
+        ${service.features.map(feature => `<li>${feature}</li>`).join('')}
+      </ul>
+      <p><strong>Price:</strong> ${service.price}</p>
+    </div>
+  `).join('');
+
+  document.getElementById("service-list").innerHTML = serviceListHTML;
+};
